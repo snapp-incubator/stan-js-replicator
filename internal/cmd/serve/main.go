d62c20dec08cc1027e8c71bdcb5a4696919b70f0
Parham Alvani <1995parham@tuta.io>
2021-09-17 15:31:39 +0430

    feat: create pipes

--- internal/cmd/serve/main.go
+++ internal/cmd/serve/main.go
@@ -3,6 +3,8 @@ package serve
 import (
 	"github.com/snapp-incubator/stan-js-replicator/internal/cmq"
 	"github.com/snapp-incubator/stan-js-replicator/internal/config"
+	"github.com/snapp-incubator/stan-js-replicator/internal/pipe"
+	"github.com/snapp-incubator/stan-js-replicator/internal/streaming"
 	"github.com/snapp-incubator/stan-js-replicator/internal/telemetry/metric"
 	"github.com/snapp-incubator/stan-js-replicator/internal/telemetry/profiler"
 	"github.com/spf13/cobra"
@@ -10,18 +12,29 @@ import (
 	"go.uber.org/zap"
 )
 
-func main(cfg config.Config, logger *zap.Logger, _ trace.Tracer) {
+func main(cfg config.Config, logger *zap.Logger, tracer trace.Tracer) {
 	profiler.Start(cfg.Telemetry.Profiler)
 
 	metric.NewServer(cfg.Telemetry.Metric).Start(logger.Named("metrics"))
 
-	c, err := cmq.New(cfg.NATS, logger)
+	c, err := cmq.New(cfg.NATS, logger.Named("cmq"))
 	if err != nil {
 		logger.Fatal("nats initiation failed", zap.Error(err))
 	}
 
 	if err := c.Stream(cfg.Channel, cfg.Topics); err != nil {
 		logger.Fatal("nats stream creation failed", zap.Error(err))
+	}
+
+	str, err := streaming.New(cfg.Streaming, logger.Named("streaming"))
+	if err != nil {
+		logger.Fatal("nats streaming initiation failed", zap.Error(err))
+	}
+
+	p := pipe.New(c, str, logger.Named("pipe"), tracer)
+
+	for _, topic := range cfg.Topics {
+		go p.Pipe(topic)
 	}
 }
 
