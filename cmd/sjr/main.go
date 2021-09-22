3097bb6a7c4fd4d2d1c3c6a38fca0cfdac7eeac6
Parham Alvani <parham.alvani@gmail.com>
2021-09-22 08:25:23 +0330

    feat: add automaxprocs

--- cmd/sjr/main.go
+++ cmd/sjr/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/snapp-incubator/stan-js-replicator/internal/cmd"
+import (
+	"github.com/snapp-incubator/stan-js-replicator/internal/cmd"
+	_ "go.uber.org/automaxprocs"
+)
 
 func main() {
 	cmd.Execute()
